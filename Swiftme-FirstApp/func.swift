758eefe41bb285b197cd7b09f052de6d86e6a464
SwiftST <pavel.zubrilov@icloud.com>
2023-04-04 20:48:24 +0500

    Добавлены функции операций

--- Swiftme-FirstApp/func.swift
+++ Swiftme-FirstApp/func.swift
@@ -7,17 +7,82 @@
 
 import Foundation
 
-func sum(_ a: String?, _ b: String?) -> String {
-    guard let oneV = a, let twoV = b else { return "Incorrect value" }
-    guard let one = Int(oneV), let two = Int(twoV) else { return "Incorrect value" }
-    let result = one + two
+let addition = { (_ numberOne: Int, _ numberTwo: Int) in
+    numberOne + numberTwo
+}
+
+let subtraction = { (_ numberOne: Int, _ numberTwo: Int) in
+    numberOne - numberTwo
+}
+
+let multiplication = { (_ numberOne: Int, _ numberTwo: Int) in
+    numberOne * numberTwo
+}
+
+let division = { (_ numberOne: Int, _ numberTwo: Int) in
+    numberOne / numberTwo
+}
+
+let exponentiation = { (_ numberOne: Int, _ numberTwo: Int) in
+    guard numberOne > 0, numberTwo > 0 else {
+        return 0
+    }
+    if numberTwo == 1 {
+        return numberOne
+    }
+    if numberTwo == 2 {
+        return numberOne * numberOne
+    }
+    return numberOne * exponentiation(numberOne, numberTwo - 1)
+}
+
+func chooseOperation(_ operation: String?) -> (Int, Int) -> Int {
+    switch operation {
+    case "1":
+        return addition
+    case "2":
+        return subtraction
+    case "3":
+        return multiplication
+    case "4":
+        return division
+    case "5":
+        return exponentiation
+    default:
+        break
+    }
+    return addition
+}
+
+func sum(_ a: String?, _ b: String?, _ closure: (Int, Int) -> Int) -> String {
+    let result = closure(Int(a!)!, Int(b!)!)
     return String(result)
 }
 
-// проверка ввода
+// проверка ввода значения
 func checkValue(_ value: inout String?) {
     while Int(value!) == nil {
         print("Некорректное значение.\nПовторите ввод")
         value = readLine()
     }
+   
 }
+
+// проверка ввода номера функции
+func checkOperation(_ value: inout String?, count: ClosedRange<Int>) {
+    for _ in 1... {
+        guard Int(value ?? "0") != nil else {
+            print("Некорректное значение.\nПовторите ввод")
+            value = readLine()
+            continue
+        }
+        guard count ~= Int(value!)! else {
+            print("Некорректное значение.\nПовторите ввод")
+            value = readLine()
+            continue
+        }
+        return
+    }
+}
+
+
--- Swiftme-FirstApp/main.swift
+++ Swiftme-FirstApp/main.swift
@@ -5,6 +5,11 @@
 //  Created by Павел Зубрилов on 26.03.2023.
 //
 
+//print("Введите количество аргументов (от 2 до 10)")
+//var countArgument = readLine()
+//checkOperation(&countArgument, count: 2...10)
+
+
 print("Введите value One")
 var valueOne = readLine() 
 checkValue(&valueOne)
@@ -13,9 +18,13 @@ print("Введите value Two")
 var valueTwo = readLine()
 checkValue(&valueTwo)
 
-print("Выберите операцию из списка")
+print("Выберите номер операции из списка:\n1. Addition\n2. Subtraction\n3. Multiplication\n4. Divison\n5. Exponentiation")
+var choseNumOperation = readLine()
+checkOperation(&choseNumOperation, count: 1...5)
+let operation = chooseOperation(choseNumOperation)
 
-let result = sum(valueOne, valueTwo)
+
+let result = sum(valueOne, valueTwo, operation)
 print("result = \(result)")
 
 
